898544b5aa4cebf033d5891df4cb33bc8844ae23
ked4ma <hades.netherworld@gmail.com>
2025-06-15 15:15:36 +0900

    update fern setting

--- nvim/lua/plugins/fern.lua
+++ nvim/lua/plugins/fern.lua
@@ -1,7 +1,35 @@
 return {
   'lambdalisue/fern.vim',
   keys = {
-     { '<C-b>', ':Fern . -reveal=% -drawer -toggle -width=40<CR>', desc = 'toggle fern' },
+    {
+      '<C-b>',
+      function()
+        local fern_win_id = nil
+
+        -- search fern window
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+          if ft == 'fern' then
+            fern_win_id = win
+            break
+          end
+        end
+
+        if fern_win_id == nil then
+          -- fern drawer is invisible -> open
+          -- Fernが開いていない → 開く
+          vim.cmd('Fern . -reveal=% -drawer -width=40<CR> -toggle')
+        elseif fern_win_id == vim.api.nvim_get_current_win() then
+          -- fern drawer is visible and focused -> close
+          vim.cmd('close')
+        else
+          -- fern drawer is visible and editing files -> focus fern drawer
+          vim.api.nvim_set_current_win(fern_win_id)
+        end
+      end,
+      desc = 'Smart toggle/focus Fern drawer',
+    },
   },
   dependencies = {
     { 'lambdalisue/nerdfont.vim' },
@@ -25,3 +53,4 @@ return {
     },
   },
 }
+
